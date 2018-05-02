82542f337423c5b1a68444d06c641f1351c974fc
D7703_24 <d7703_24@oa.dit.ac.kr>
2018-05-02 11:23:58 +0900

    Initial Commit

---
+++ Simple TableView 01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple TableView 01
+//
+//  Created by D7703_24 on 2018. 5. 2..
+//  Copyright © 2018년 D7703_24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
